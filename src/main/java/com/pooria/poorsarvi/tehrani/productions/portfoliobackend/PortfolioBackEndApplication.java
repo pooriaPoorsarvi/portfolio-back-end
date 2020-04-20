54dca36c112334fa06b831181eb82457c0bfd8b4
poori <pooria.poorsarvi.tehrani@mail.utoronto.ca>
2020-04-20 00:02:35 -0400

    INIT

---
+++ src/main/java/com/pooria/poorsarvi/tehrani/productions/portfoliobackend/PortfolioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.pooria.poorsarvi.tehrani.productions.portfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pooria/poorsarvi/tehrani/productions/portfoliobackend/PortfolioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooria.poorsarvi.tehrani.productions.portfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
