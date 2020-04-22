2ba8285c1e443ef273057c97d95a7a20a11bd5c8
poori <pooriapoorsarvi@gmail.com>
2020-04-22 03:38:16 -0400

    controller cleaned

--- src/main/java/com/pooria/poorsarvi/tehrani/productions/portfoliobackend/app/blog/BlogPostController.java
+++ src/main/java/com/pooria/poorsarvi/tehrani/productions/portfoliobackend/app/blog/BlogPostController.java
@@ -3,7 +3,6 @@ package com.pooria.poorsarvi.tehrani.productions.portfoliobackend.app.blog;
 import com.pooria.poorsarvi.tehrani.productions.portfoliobackend.exceptions.EntityNotFoundInDBException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
