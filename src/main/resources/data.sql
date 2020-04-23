54d7180f79919bbc8c12aa6f06dd035ce4366d04
poori <pooriapoorsarvi@gmail.com>
2020-04-23 02:12:00 -0400

    init md s are now in s3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ insert into blog_post_entity values (1, 'https://s3.amazonaws.com/prod-www-black
       <h4>Machine learning paper</h4>
       <p class="text-black-50 mb-0">One of the papers worked, was the use of machine learning inorder to find code vulnerabilities by a Neural Network model. This model used RNN and text processing and the paper can be found <a href="https://www.sciencedirect.com/science/article/abs/pii/S0957417419308735">here</a>.</p>
   </div>',
-        'assets/test.md'
+        'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/test.md'
         , '<i>Blog post title1:</i>');
 insert into blog_post_entity values (2, 'https://s3.amazonaws.com/prod-www-blackline-com/blog/wp-content/uploads/2019/01/29163118/A-Conversation-With-BlackLines-Machine-Learning-Experts.jpg', '<div class="featured-text text-center text-lg-left">
       <h4>Machine learning paper</h4>
       <p class="text-black-50 mb-0">One of the papers worked, was the use of machine learning inorder to find code vulnerabilities by a Neural Network model. This model used RNN and text processing and the paper can be found <a href="https://www.sciencedirect.com/science/article/abs/pii/S0957417419308735">here</a>.</p>
-  </div>', 'assets/test.md', '<h1><i>Blog post title2:</i></h1><br>');
+  </div>', 'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/test.md', '<h1><i>Blog post title2:</i></h1><br>');
