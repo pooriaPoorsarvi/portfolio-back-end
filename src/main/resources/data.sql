0fca8e7cb789d40e9419fb0d04a80a3f4a51e4c1
poori <pooriapoorsarvi@gmail.com>
2020-04-29 01:59:14 -0400

    first blog post init updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into blog_post_entity values (1, 'https://s3.amazonaws.com/prod-www-blackline-com/blog/wp-content/uploads/2019/01/29163118/A-Conversation-With-BlackLines-Machine-Learning-Experts.jpg', '<div class="featured-text text-center text-lg-left">
-      <h4>Machine learning paper</h4>
-      <p class="text-black-50 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+insert into blog_post_entity values (1, 'https://www.advantageservices.net/photos/articles/correct/gettyimages-881467948.jpg', '<div class="featured-text text-center text-lg-left">
+      <h4>Intro to the making of my website</h4>
+      <p class="text-black-50 mb-0">Learn all about how I made this website, and what technologies were used. This is just a big summary of all the technologies.</p>
     </div>',
-        'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/test.md'
-        , '<i>Blog post title1:</i>');
+        'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/testIntro.md'
+        , '<i>Intro to the making of my website</i>');
 insert into blog_post_entity values (2, 'https://s3.amazonaws.com/prod-www-blackline-com/blog/wp-content/uploads/2019/01/29163118/A-Conversation-With-BlackLines-Machine-Learning-Experts.jpg',
     '<div class="featured-text text-center text-lg-left">
       <h4>Machine learning paper</h4>
