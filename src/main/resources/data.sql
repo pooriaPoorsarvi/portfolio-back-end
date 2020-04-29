d322fefb9eda8f540c8843f7e3699d5e4214adc6
poori <pooriapoorsarvi@gmail.com>
2020-04-29 03:00:14 -0400

    first blog post init updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,3 @@ insert into blog_post_entity values (1, 'https://www.advantageservices.net/photo
     </div>',
         'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/testIntro.md'
         , '<i>Intro to the making of my website</i>');
-# insert into blog_post_entity values (2, 'https://s3.amazonaws.com/prod-www-blackline-com/blog/wp-content/uploads/2019/01/29163118/A-Conversation-With-BlackLines-Machine-Learning-Experts.jpg',
-#     '<div class="featured-text text-center text-lg-left">
-#       <h4>Machine learning paper</h4>
-#       <p class="text-black-50 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-#     </div>', 'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/test.md', '<h1><i>Blog post title2:</i></h1><br>');
