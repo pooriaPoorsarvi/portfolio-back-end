1e0b874403bb0a9cac7b28e20e76d321eb685558
poori <pooriapoorsarvi@gmail.com>
2020-10-14 03:23:29 -0400

    blog git added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,14 @@ select 1, 'https://www.advantageservices.net/photos/articles/correct/gettyimages
 where not exists (SELECT * FROM blog_post_entity WHERE id = 1) ;
 
 
+
+insert into blog_post_entity (id, image_src, intro, mark_down_src, title)
+select 2, 'https://www.linode.com/docs/development/version-control/introduction-to-version-control/1204-image_versioning_intro_1.jpg', '<div class="featured-text text-center text-lg-left">
+      <h4>Intro to the making of your own versioning system</h4>
+      <p class="text-black-50 mb-0">Learn all about how I made own serve hosted versioning system</p>
+    </div>',
+       'https://ppt-productions-blog-posts.s3.us-east-2.amazonaws.com/gitBlog.md'
+        , '<i>Intro to making of my self hosted versioning system.</i>'
+where not exists (SELECT * FROM blog_post_entity WHERE id = 2) ;
+
+
